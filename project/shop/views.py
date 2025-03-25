e4f81d7137921e3a4280a9811efa53bc29cd592a
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-03-25 20:04:01 +0100

    filtering, return product detail

--- project/shop/views.py
+++ project/shop/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 
 def home(request):
     products = Product.objects.all()
+    category_name = request.GET.get("category")
+    filter = request.GET.get("filter")
+    categories = Category.objects.all()
 
-    return render(request,"index.html", {"products":products})
+    if category_name:
+        category = get_object_or_404(Category, name=category_name)
+        products = products.filter(category=category)
+
+    if filter == "decrease_price":
+        products=products.order_by("-price")
+    elif filter == "increase_price":
+        products=products.order_by("price")
+    elif filter == "increase_rating":
+        products=products.order_by("rating")
+    else:
+        products=products.order_by("-rating")
+
+    return render(request,"index.html", {"products":products,"categories": categories})
 
 
 def about(request):
     return render(request, "about.html")
+
+
+def product_details(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    return render(request, "product_detail.html", {"product":product})
