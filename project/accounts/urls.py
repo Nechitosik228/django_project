4fcc8a99c4e720fb62f180565b57b2e2a0009e3b
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-04-03 22:41:56 +0200

    add authorization, profile

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import register
+from .views import register, login_view, logout_view, profile
 
 app_name = "accounts"
 
 urlpatterns = [
-    path('register/', register, name='register')
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('profile/', profile, name='profile')
 ]
--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
 
 
@@ -14,3 +15,25 @@ def register(request):
         form = RegisterForm()
 
     return render(request, 'register.html', {'form':form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            next_url = request.GET.get('next')
+            return redirect(next_url or 'shop:home')
+        else:
+            return render(request, 'login.html', {'error': 'Incorrect login or password'})
+    else:
+        return render(request, 'login.html')
+    
+def logout_view(request):
+    logout(request)
+    return redirect('shop:home')
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
