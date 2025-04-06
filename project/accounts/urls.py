b40f6bc8a841038f2445fe93aa11c63e7177b467
Nechitosik228 <nikitanechitailo93@gmail.com>
2025-04-06 22:35:22 +0300

    django homework done, add password change

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from .views import register, login_view, logout_view, profile
 
 app_name = "accounts"
@@ -8,6 +9,6 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('profile/', profile, name='profile'),
-    path('password-change/', name='password_change'),
-    path('password-change/done/', name='password_change_done')
+    path('password-change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy("accounts:password_change_done"), template_name="password_change.html"), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done')
 ]
